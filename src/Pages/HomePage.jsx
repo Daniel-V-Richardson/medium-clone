9282c78bb7062d9b9fa367fdc9e0022e50fb8493
Daniel-V-Richardson <richardson20june@gmail.com>
2023-07-17 16:49:59 +0530

    Fixed Bugs

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -46,10 +46,10 @@ const HomePage = () => {
   }
 
   return (
-    <div className="home-container">
+  <div className="home-container">
       <Navbar userData={userData} />
-      {/* <Banner /> */}
-      <Spline  scene="https://prod.spline.design/WTl0uo3oqQuNt3pf/scene.splinecode" />
+      <Banner />
+      {/* <Spline  scene="https://prod.spline.design/WTl0uo3oqQuNt3pf/scene.splinecode" /> */}
       <div className=" font-black text-5xl flex md:ml-[120px]  sm:ml-0 mt-4 md:mt-[50px] ] ">
         Latest Posts
       </div>
